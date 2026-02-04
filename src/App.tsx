526e76e0cc6179b55a2c381fc143a5c28c710433
Delon Pratama <delonpratama1009@gmail.com>
2026-02-04 18:47:55 +0700

    add routing pages skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
-import Login from "@/pages/Login";
+import { Login, Quiz, Result } from "@/pages";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/quiz" element={<Quiz />} />
+      <Route path="/result" element={<Result />} />
     </Routes>
   );
 }
---
+++ src/pages/index.ts
@@ -0,0 +1,3 @@
+export { default as Quiz } from "./Quiz";
+export { default as Login } from "./Login";
+export { default as Result } from "./Result";
