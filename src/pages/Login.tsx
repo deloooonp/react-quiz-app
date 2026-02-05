7ee66adac8e10baab037e7ea9b97583af3ed4359
Delon Pratama <delonpratama1009@gmail.com>
2026-02-05 14:41:28 +0700

    simple login logic using localStorage

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,29 @@
+import { useState } from "react";
+import { useNavigate } from "react-router";
+
 export default function Login() {
-  return <div>Login</div>;
+  const [username, setUsername] = useState("");
+  const navigate = useNavigate();
+
+  const handleStart = () => {
+    if (!username.trim()) return;
+
+    localStorage.setItem("username", username);
+    navigate("/quiz");
+  };
+
+  return (
+    <div>
+      <h1>Quiz App</h1>
+
+      <input
+        type="text"
+        placeholder="Enter your name"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+
+      <button onClick={handleStart}>Start Quiz</button>
+    </div>
+  );
 }
