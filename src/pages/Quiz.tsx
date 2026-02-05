b1feeb8880b4080bcb8e9e1ae56f4e0cf53353d1
Delon Pratama <delonpratama1009@gmail.com>
2026-02-05 14:47:02 +0700

    protect quiz route with login

--- src/pages/Quiz.tsx
+++ src/pages/Quiz.tsx
@@ -1,3 +1,16 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function Quiz() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const username = localStorage.getItem("username");
+
+    if (!username) {
+      navigate("/");
+    }
+  });
+
   return <div>Quiz Page</div>;
 }
